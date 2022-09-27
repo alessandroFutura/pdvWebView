70e8ab38a92d26ac66bebd22f71eb692fa71f330
Edson Cássio Barcelos <edsonbarcellos02@gmail.com>
2022-09-27 08:06:59 -0300

    1.0.0.0

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style/index.css';
+import Pdv from './pages/checkout';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Pdv/>
+  </React.StrictMode>
+);
+
